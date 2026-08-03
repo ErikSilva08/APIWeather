4822d57f9eb173228a328a17d73b732debac9a0f
ErikSilva08 <eriksilvaf89@gmail.com>
2026-08-03 12:01:06 -0300

    first commit

---
+++ src/main/java/com/silvaSoftware/ClimaDoJogo/ClimaDoJogoApplication.java
@@ -0,0 +1,13 @@
+package com.silvaSoftware.ClimaDoJogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimaDoJogoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClimaDoJogoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/silvaSoftware/ClimaDoJogo/ClimaDoJogoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.silvaSoftware.ClimaDoJogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimaDoJogoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
